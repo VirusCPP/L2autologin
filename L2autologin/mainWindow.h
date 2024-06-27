83f3c3172ac6f7b75cba78efa1aee6a85a39e942
Александр Чувилов <alex_chuvilov@mail.ru>
2024-06-27 10:52:24 +0500

    0.8

--- L2autologin/mainWindow.h
+++ L2autologin/mainWindow.h
@@ -25,7 +25,7 @@ namespace L2autologin {
 			checkPathStatus();
 		}
 		static int _delay;
-		static String^ _version = L"0.7";
+		static String^ _version = L"0.8";
 		void removeAccButton_Click(System::Object^ sender, System::EventArgs^ e);
 		void addAccButton_Click(System::Object^ sender, System::EventArgs^ e);
 		static void addAccount(String^ name, String^ login, String^ password);
